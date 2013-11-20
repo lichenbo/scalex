eb27d3167c210084aaa711b13f1b5dc7485537e9
Chenbo Li <lichenbo1949@gmail.com>
2013-11-20 21:13:50 +0800

    add test

---
+++ src/ExprExampleSuite.scala
@@ -0,0 +1,8 @@
+import org.junit.Assert._
+import org.junit.Test
+import org.junit.Before
+import org.scalatest._
+
+class ExprExampleSuite {
+
+}
--- src/ParserExpr.scala
+++ src/ParserExpr.scala
@@ -9,14 +9,35 @@ case class Literal(chr:Char) extends Expr
 
 object ParserExpr {
 	def main(args:Array[String]) {
-		RegexDef.check("ab|c")
+	  println(RegexDef.check("abc"))
+	  println(RegexDef.check("ab|c"))
+	  println(RegexDef.check("a|bc"))
+	  println(RegexDef.check("abc|def|g"))
+	  println(RegexDef.check("a"))
+	  println(RegexDef.check("a*"))
+	  println(RegexDef.check("ab*"))
+	  println(RegexDef.check("a*b"))
+	  println(RegexDef.check("a*b*"))
+	  println(RegexDef.check("a**"))
+	  println(RegexDef.check("a*|b"))
+	  println(RegexDef.check("a|b*"))
+	  println(RegexDef.check("a*|b*"))
+	  println(RegexDef.check("(a)"))
+	  println(RegexDef.check("(abc)"))
+	  println(RegexDef.check("(abc|def|g)"))
+	  println(RegexDef.check("(ab*)"))
+	  println(RegexDef.check("(ab)c"))
+	  println(RegexDef.check("a(bc)"))
+	  println(RegexDef.check("a|b(cd)"))
+	  println(RegexDef.check("a|(c|d)"))
 	} 
 }
 
 object RegexDef {
 	val symbol_stack: Stack[Char] = new Stack
 	val char_stack: Stack[Expr] = new Stack
-	private val pri:Map[Char,Int] = Map('*'->3,'|'->2,'+'->1)
+	private val pri:Map[Char,Int] = Map('*'->3,'|'->2,'+'->1,'('->0)
+	private var canConcat = false
 
 	val token_pattern = "([a-zA-Z_])".r
 	private def canEvaluate (before:Char, current:Char) = pri(before)>=pri(current) 
@@ -44,24 +65,30 @@ object RegexDef {
 		for (regchar <- reg.toList) {
 			regchar.toString match {    
 			case token_pattern(c) => {
-				while(symbol_stack.length > 0 && char_stack.length > 1 &&  symbol_stack.top != '|' && canEvaluate(symbol_stack.top, '+')) {
+				while(symbol_stack.length > 0 && canConcat && canEvaluate(symbol_stack.top, '+')) {
 					eval(symbol_stack.pop)
 				}
-				char_stack.push(Literal(c(0)))
-				if (char_stack.length > 1)
+				if (char_stack.length > 0 && canConcat)
 					symbol_stack.push('+')
+				char_stack.push(Literal(c(0)))
+				canConcat = true
 			}
 			case "*" => {
 				while(symbol_stack.length > 0 && canEvaluate(symbol_stack.top,regchar)) {
 					eval(symbol_stack.pop)
 				}
 				symbol_stack.push('*')
+				canConcat = true
 			}
 			case "(" => {
 				while(symbol_stack.length > 0 && canEvaluate(symbol_stack.top,regchar)) {
 					eval(symbol_stack.pop)
 				}
+				if (canConcat) {
+				  symbol_stack.push('+')
+				}
 				symbol_stack.push('(')
+				canConcat = false
 			}
 			case ")" => {
 				while(symbol_stack.top != '(') {
@@ -69,12 +96,14 @@ object RegexDef {
 				}
 				assert(symbol_stack.top == '(')
 				symbol_stack.pop
+				canConcat = true
 			}
 			case "|" => {
 				while(symbol_stack.length > 0 && canEvaluate(symbol_stack.top,regchar)) {
 					eval(symbol_stack.pop)
 				}
 				symbol_stack.push('|')
+				canConcat = false
 			}
 			}
 			println("Symbol_stack:" + symbol_stack)
@@ -83,7 +112,12 @@ object RegexDef {
 		/* Assure the last calc */
 		while (symbol_stack.length > 0) 
 		  eval(symbol_stack.pop)
-		println(char_stack)
-		println(symbol_stack)
+		
+		println("Final symbol:" + symbol_stack)
+		println("Final char:" + char_stack)
+		assert(symbol_stack.length == 0)
+		assert(char_stack.length == 1)
+		canConcat = false
+		return char_stack.pop
 	}
 }
