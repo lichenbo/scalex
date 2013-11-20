e2f9a1704dd71b8726e86c225afd71ac27b4b498
Chenbo Li <lichenbo1949@gmail.com>
2013-11-20 19:58:51 +0800

    another commit

--- src/ParserExpr.scala
+++ src/ParserExpr.scala
@@ -9,7 +9,7 @@ case class Literal(chr:Char) extends Expr
 
 object ParserExpr {
 	def main(args:Array[String]) {
-		RegexDef.check("abc")
+		RegexDef.check("ab|c")
 	} 
 }
 
@@ -20,22 +20,25 @@ object RegexDef {
 
 	val token_pattern = "([a-zA-Z_])".r
 	private def canEvaluate (before:Char, current:Char) = pri(before)>=pri(current) 
-	private def eval (c:Char) = c match {
-	case '+' => {
-		val op2 = char_stack.pop
+	private def eval (c:Char) = {
+			c match {
+			case '+' => {
+				val op2 = char_stack.pop
 				val op1 = char_stack.pop
 				char_stack.push(Concat(op1,op2))
-	}
-	case '*' => {
-		val op = char_stack.pop
+			}
+			case '*' => {
+				val op = char_stack.pop
 				char_stack.push(Star(op))
-	}
-	case '|' => {
-		val op2 = char_stack.pop
+			}
+			case '|' => {
+				val op2 = char_stack.pop
 				val op1 = char_stack.pop
 				char_stack.push(Union(op1,op2))
+			}
+			}
+			println("CharStack:" + char_stack)
 	}
-	}; println("CharStack:" + char_stack)
 
 	def check(reg:String) {
 		for (regchar <- reg.toList) {
@@ -45,7 +48,8 @@ object RegexDef {
 					eval(symbol_stack.pop)
 				}
 				char_stack.push(Literal(c(0)))
-				symbol_stack.push('+')
+				if (char_stack.length > 1)
+					symbol_stack.push('+')
 			}
 			case "*" => {
 				while(symbol_stack.length > 0 && canEvaluate(symbol_stack.top,regchar)) {
@@ -75,6 +79,10 @@ object RegexDef {
 			}
 			println("Symbol_stack:" + symbol_stack)
 		}
+		
+		/* Assure the last calc */
+		while (symbol_stack.length > 0) 
+		  eval(symbol_stack.pop)
 		println(char_stack)
 		println(symbol_stack)
 	}
