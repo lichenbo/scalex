6654d3c8278ab260d0fed30d351634fc25eb2e37
Chenbo Li <lichenbo1949@gmail.com>
2013-11-22 04:33:20 +0800

    first try of eDFA

--- src/Reg2DFA.scala
+++ src/Reg2DFA.scala
@@ -1,32 +1,59 @@
-
+import scala.collection.mutable
 
 object Reg2eDFA {
   def main(args:Array[String]) {
-    
+    println(convert(RegexDef.check("a|bc")))
   }
   
-  def convert(expr:Expr) = expr match {
-    case Literal(c) => {
+  def convert(expr:Expr):StateGraph = expr match {
+    case Literal(c: Char) => {
       val s1 = new State
       val s2 = new State
-      s1.relationMap = Map[Char,List[State]]()
-      val graph = new StateGraph (List(new S))
-
+      s1.relationMap addBinding (c,s2)
+      new StateGraph (List(s1,s2),s1,s2)
     }
-    case Union(u1,u2) => ???
-    case Concat(c1,c2) => ??? 
-    case Star(s) => ???
+    case Union(e1: Expr,e2: Expr) => {
+      val s1 = new State
+      val s2 = new State
+      s1.relationMap addBinding('@',convert(e1).startState)
+      s1.relationMap addBinding ('@',convert(e2).startState)
+      convert(e1).endState.relationMap addBinding ('@',s2)
+      convert(e2).endState.relationMap addBinding ('@',s2)
+      new StateGraph(List(s1,s2):::convert(e1).list:::convert(e2).list, s1, s2)
+    }
+    case Concat(e1,e2) => {
+      val s1 = new State
+      val s2 = new State
+      s1.relationMap addBinding ('@', convert(e1).startState)
+      convert(e2).endState.relationMap addBinding ('@', s2)
+      new StateGraph(List(s1,s2):::convert(e1).list:::convert(e2).list, s1, s2)
+    }
+    case Star(e) => {
+      val s1 = new State
+      val s2 = new State
+      val s3 = new State
+      val s4 = new State
+      s1.relationMap addBinding ('@', s2)
+      s2.relationMap addBinding ('@', convert(e).startState)
+      convert(e).endState.relationMap addBinding ('@', s3)
+      s3.relationMap addBinding ('@', s2)
+      s3.relationMap addBinding ('@', s4)
+      new StateGraph(List(s1,s2,s3,s4):::convert(e).list, s1, s4)
+    }
   }
   
-  def combine(sg1:StateGraph, sg2:StateGraph):StateGraph = ???
 }
 
-class State (){
-  val relationMap:Map[Char,List[State]] = Map[Char,List[State]]()
-  
+class State {
+  val relationMap = new mutable.HashMap[Char,mutable.Set[State]] with mutable.MultiMap[Char,State]
+  override def toString() = {
+    relationMap.mkString
+  }
 }
 
-class StateGraph(list:List[State],startState:State,endState:State){ 
-  
+class StateGraph(val list:List[State], val startState:State, val endState:State){ 
+  override def toString() = {
+    startState.toString
+  }
 }
 
